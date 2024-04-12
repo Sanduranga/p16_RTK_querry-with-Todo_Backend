250966328ec2d30d924437520eebddb1ee1982ce
Sanduranga <edirisinghest527@gmail.com>
2024-04-12 23:33:49 +0530

    set up get api

--- app.js
+++ app.js
@@ -11,13 +11,19 @@ app.use(
 );
 app.use(express.json());
 
-app.post("/todos", (req, res) => {
+app.post("/todo", (req, res) => {
   const user = new User({
     id: req.body.id,
     task: req.body.task,
   });
   user
     .save()
+    .then((responce) => res.json({ responce }))
+    .catch((err) => res.json({ err }));
+});
+
+app.get("/todos", (req, res) => {
+  User.find()
     .then((responce) => res.json({ responce }))
     .catch((err) => res.json({ err }));
 });
