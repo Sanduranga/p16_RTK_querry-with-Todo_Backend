c24f1e7b90f37a28ec8c2715434938124193defc
Sanduranga <edirisinghest527@gmail.com>
2024-04-13 16:08:47 +0530

    made delete and edite api's also

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const User = require("./models/todoModel");
+const Task = require("./models/todoModel");
 
 app.use(cors());
 app.use(
@@ -12,19 +12,33 @@ app.use(
 app.use(express.json());
 
 app.post("/todo", (req, res) => {
-  const user = new User({
+  const user = new Task({
     id: req.body.id,
     task: req.body.task,
   });
   user
     .save()
-    .then((responce) => res.json({ responce }))
+    .then((response) => res.json({ response }))
     .catch((err) => res.json({ err }));
 });
 
 app.get("/todos", (req, res) => {
-  User.find()
-    .then((responce) => res.json({ responce }))
+  Task.find()
+    .then((response) => res.json({ response }))
+    .catch((err) => res.json({ err }));
+});
+
+app.delete("/delete_todo", (req, res) => {
+  const id = req.body.id;
+  Task.deleteOne({ _id: id })
+    .then((response) => res.json({ response }))
+    .catch((err) => res.json({ err }));
+});
+
+app.put("/update_todo", (req, res) => {
+  const { _id, id, task } = req.body;
+  Task.findByIdAndUpdate({ _id: _id }, { id: id, task: task })
+    .then((response) => res.json({ response }))
     .catch((err) => res.json({ err }));
 });
 
